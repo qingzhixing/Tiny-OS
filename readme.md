dc35e8e57cccc75fe9fbdd8e2fd8044ad15ffe91
qingzhixing <qzsqqmail@qq.com>
2023-01-10 19:51:47 +0800

    add references

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
  操作系统真象还原学习
 
 希望能够坚持学下去！
+
+## git clone之后要做的事
+在references下运行donwload-references.sh脚本下载参考项目，当然也可以不下载(
---
+++ references/download-references.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Cooi-Boi/Tiny-OS.git
+git clone https://gitee.com/qingzhixing/OsElephant.git
