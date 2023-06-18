0f2e01a6529e15df859e08a53bec8da6fb11aa50
qingzhixing <qzsqqmail@qq.com>
2023-06-18 23:15:22 +0800

    put_int_hex完成！

--- src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
+++ src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
@@ -1,8 +1,9 @@
 #include "print.h"
 int main(void)
 {
-    char message[] = "Kernel Message:put_str();";
+    char message[] = "Kernel Message:put_str();\n";
     put_str(message);
+    put_int_hex(0xFFFFFFFF);
     while (1)
     {
         // 阻塞
--- src/class-6完善内核/6.3实现自己的打印函数/kernel/print.h
+++ src/class-6完善内核/6.3实现自己的打印函数/kernel/print.h
@@ -3,4 +3,5 @@
 #include "stdint.h"
 extern void put_char(uint8_t char_asci);
 extern void put_str(char *str);
+extern void put_int_hex(uint32_t num_hex);
 #endif /* __LIB_KERNEL_PRINT_H */
