0338cc9992f90ca4b1d86660d1d33c94a7439526
qingzhixing <qzsqqmail@qq.com>
2023-06-18 20:08:03 +0800

    makefile调试完毕，等待debug

--- src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
+++ src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
@@ -1,10 +1,9 @@
 #include "print.h"
-void main(void)
+int main(void)
 {
-    uint8_t kernelMessage[] = "kernel message\n";
-    uint8_t pointer = 0;
-    while (kernelMessage[pointer] != '\0')
+    put_char('k');
+    while (1)
     {
-        put_char(kernelMessage[pointer++]);
+        // 阻塞
     }
 }
