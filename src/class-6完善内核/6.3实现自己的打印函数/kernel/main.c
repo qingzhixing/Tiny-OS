2bffa46e7f1979d9814ffe54dcfbfee8a2e9dc82
qingzhixing <qzsqqmail@qq.com>
2023-06-18 21:58:09 +0800

    成功在kernel中使用put_char函数！

--- src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
+++ src/class-6完善内核/6.3实现自己的打印函数/kernel/main.c
@@ -1,7 +1,12 @@
 #include "print.h"
 int main(void)
 {
-    put_char('k');
+    uint8_t message[] = "Kernel Message:put_char();\n";
+    uint8_t pointer = 0;
+    while (message[pointer] != '\0')
+    {
+        put_char(message[pointer++]);
+    }
     while (1)
     {
         // 阻塞
