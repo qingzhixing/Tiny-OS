d8ad6c8c421917ff0fd727d80c668a48428a8a15
qingzhixing <qzsqqmail@qq.com>
2023-07-19 22:09:35 +0800

    正在编写memory.c,更改TODO的问题

--- src/class-8内存管理系统/8.5内存管理系统/kernel/memory.c
+++ src/class-8内存管理系统/8.5内存管理系统/kernel/memory.c
@@ -10,6 +10,7 @@
  * 0xc009f00是内核主线程栈顶，0xc009e00是内核主线程的pcb
  * 一个页框大小(4kB)的位图可表示128MB的内存：4kByte = 32kbit => 32k*4kB = 128MB
  * 位图地址安排在0xc009a00,这样本系统最大支持4个页框的位图即512MB
+ * 0xc009e00-0x4000(4kB)=0xc009a00
  */
 #define MEM_BITMAP_BASE 0xc009a00
 
@@ -28,3 +29,27 @@ struct pool
 };
 
 struct pool kernel_pool, user_pool; // 生成内核内存池与用户内存池
+
+struct virtual_addr kernel_vaddr; // 给内核分配虚拟地址
+
+/*
+ * 初始化内存池
+ * all_mem: 内存总量
+ */
+static void mem_pool_init(uint32_t all_mem)
+{
+    put_str("   mem_pool_init start\n");
+
+    // 页表大小 = 1页的页目录项 + 第0和第768个页目录项指向同一个页表 +
+    //  第769~1022个页目录项共指向254个页表，一共有256个页表
+    //  故大小为256*PG_SIZE=0x20_0000字节(2MB)
+    uint32_t page_table_size = PG_SIZE * 256;
+
+    // 0x10_0000为低端1MB内存
+    uint32_t used_mem = page_table_size + 0x10_0000;
+
+    uint32_t free_mem = all_mem - used_mem;
+
+    // 1页为4kB,不管内存是不是4k的倍数，对于以页位单位的内存分配策略，不足一页的内存就不用考虑了
+    uint16_t all_free_page = free_mem / PG_SIZE;
+}
