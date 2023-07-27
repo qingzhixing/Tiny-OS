09de873525a7bd0dba19063e0d4c79a292423dd9
qingzhixing <qzsqqmail@qq.com>
2023-07-28 02:25:17 +0800

    memory.h

--- src/class-8内存管理系统/8.5内存管理系统/8.5.2分配页内存/kernel/memory.h
+++ src/class-8内存管理系统/8.5内存管理系统/8.5.2分配页内存/kernel/memory.h
@@ -12,4 +12,26 @@ extern struct pool kernel_pool, user_pool;
 void mem_init(void);
 void print_pool_info(struct pool *);
 void print_vaddr_info(struct virtual_addr *);
+
+// 内存池标记，用于判断使用哪个内存池
+enum pool_flags
+{
+    PF_KERNEL = 1, // 内核内存池
+    PF_USER = 2    // 用户内存池
+};
+
+// 以下属性的值是按位次来定义的，并不是0和1
+// 页存在位：存在
+#define PG_P_1 1
+// 页存在位：不存在
+#define PG_P_0 0
+// R/W位 = 0: 读/执行
+#define PG_RW_R 0
+// R/W位 = 1: 读/写/执行
+#define PG_RW_W 2
+// U/S位 = 0: 系统级
+#define PG_US_S 0
+// U/S位 = 1: 用户级
+#define PG_US_U 4
+
 #endif /* __KERNEL_MEMORY_H */
